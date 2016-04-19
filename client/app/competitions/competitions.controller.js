1a22d0dd9a176a5846265610f6b12bc350e11441
Alejandro López <a.lopez.sanchez@udc.es>
2016-04-19 22:05:30 +0200

    implementing dialog

--- client/app/competitions/competitions.controller.js
+++ client/app/competitions/competitions.controller.js
@@ -1,6 +1,38 @@
 'use strict';
 
 angular.module('agfaWebappApp')
-  .controller('CompetitionsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('CompetitionsCtrl', function ($scope, $mdDialog) {
+  	$scope.message = 'Hello';
+	
+	$scope.status = '  ';
+	  
+	$scope.showAdvanced = function(event) {
+               $mdDialog.show({
+                  clickOutsideToClose: true,
+                  scope: $scope,        
+                  preserveScope: true,           
+                  template: '<md-dialog>' +
+                              '  <md-dialog-content>' +
+                              '     Welcome to TutorialsPoint.com' +
+                              '  </md-dialog-content>' +
+                              '</md-dialog>',
+                  controller: function DialogController($scope, $mdDialog) {
+                     $scope.closeDialog = function() {
+                        $mdDialog.hide();
+                     }
+                  }
+               });
+            };
+	
+	/*function DialogController($scope, $mdDialog) {
+		$scope.hide = function() {
+			$mdDialog.hide();
+		};
+		$scope.cancel = function() {
+			$mdDialog.cancel();
+		};
+		$scope.answer = function(answer) {
+			$mdDialog.hide(answer);
+		};
+	};*/
   });
