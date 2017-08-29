4697ec22e104fa8a91e5eb2c5722a35a132d56d1
Alejandro López <a.lopez.sanchez@outlook.es>
2017-08-29 22:21:06 +0200

    Navbar changes

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,50 +1,50 @@
 'use strict';
 
-var NavbarController = function($scope, Auth, $mdSidenav, $translate, appConfig) {
+var NavbarController = function ($scope, Auth, $mdSidenav, $translate, appConfig) {
 
-    $translate(['app.components.navbar.users', 'app.components.navbar.competitions', 'app.components.navbar.teams', 'app.components.navbar.fields'])
-        .then(values => {
-            this.menu = [{
-                'title': values['app.components.navbar.users'],
-                'state': 'admin',
-                'icon': 'person'
-            }, {
-                'title': values['app.components.navbar.competitions'],
-                'state': 'competitions',
-                'icon': 'competition'
-            }, {
-                'title': values['app.components.navbar.teams'],
-                'state': 'teams',
-                'icon': 'helmet'
-            }, {
-                'title': values['app.components.navbar.fields'],
-                'state': 'fields',
-                'icon': 'field'
-            }];
-        });
+	$translate(['app.components.navbar.users', 'app.components.navbar.competitions', 'app.components.navbar.teams', 'app.components.navbar.fields'])
+		.then(values => {
+			this.menu = [{
+				'title': values['app.components.navbar.fields'],
+				'state': 'fields',
+				'icon': 'field'
+			}, {
+				'title': values['app.components.navbar.competitions'],
+				'state': 'competitions',
+				'icon': 'competition'
+			}, {
+				'title': values['app.components.navbar.teams'],
+				'state': 'teams',
+				'icon': 'helmet'
+			}, {
+				'title': values['app.components.navbar.users'],
+				'state': 'admin',
+				'icon': 'person'
+			}];
+		});
 
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    Auth.getCurrentUser((user) => {
-        this.user = user;
-    });
+	this.isLoggedIn = Auth.isLoggedIn;
+	this.isAdmin = Auth.isAdmin;
+	Auth.getCurrentUser((user) => {
+		this.user = user;
+	});
 
-    this.imagesServer = appConfig.imagesServer;
+	this.imagesServer = appConfig.imagesServer;
 
-    $scope.showMobileMainHeader = true;
-    this.openSideNavPanel = function() {
-        $mdSidenav('left').open();
-    };
-    this.closeSideNavPanel = function() {
-        $mdSidenav('left').close();
-    };
+	$scope.showMobileMainHeader = true;
+	this.openSideNavPanel = function () {
+		$mdSidenav('left').open();
+	};
+	this.closeSideNavPanel = function () {
+		$mdSidenav('left').close();
+	};
 
-    this.toggleSideNavPanel = function() {
-        $mdSidenav('left').toggle();
-    };
+	this.toggleSideNavPanel = function () {
+		$mdSidenav('left').toggle();
+	};
 
 };
 
 angular
-    .module('agfaWebappApp')
-    .controller('NavbarController', NavbarController);
+	.module('agfaWebappApp')
+	.controller('NavbarController', NavbarController);
